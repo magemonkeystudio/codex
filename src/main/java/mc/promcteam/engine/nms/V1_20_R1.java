228fc7c39cdcf7fb690379e993a7f98d360b1afe
dgafcb09Sentropic <sentropic.sv@gmail.com>
2023-06-16 03:36:44 -0600

    Reflection fixes for 1.20

--- src/main/java/mc/promcteam/engine/nms/V1_20_R1.java
+++ src/main/java/mc/promcteam/engine/nms/V1_20_R1.java
@@ -1,5 +1,10 @@
 package mc.promcteam.engine.nms;
 
+import mc.promcteam.engine.utils.Reflex;
+import org.bukkit.entity.Player;
+
+import java.lang.reflect.Method;
+
 public class V1_20_R1 implements NMS {
 
 }
