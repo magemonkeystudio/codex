de53edcd248e6574929b898d4451a8903006cd58
Travja <the.only.t.craft@gmail.com>
2024-04-05 00:48:14 -0600

    Code cleanup

--- src/main/java/studio/magemonkey/codex/manager/api/task/ITask.java
+++ src/main/java/studio/magemonkey/codex/manager/api/task/ITask.java
@@ -1,24 +1,21 @@
 package studio.magemonkey.codex.manager.api.task;
 
-import studio.magemonkey.codex.CodexPlugin;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
+import studio.magemonkey.codex.CodexPlugin;
 
+@RequiredArgsConstructor
 public abstract class ITask<P extends CodexPlugin<P>> {
-
     @NotNull
-    protected P       plugin;
-    protected int     id;
-    protected long    interval;
-    protected boolean async;
+    protected final P       plugin;
+    protected final long    interval;
+    protected final boolean async;
+    @Getter
+    protected       int     id;
 
     public ITask(@NotNull P plugin, int interval, boolean async) {
         this(plugin, interval * 20L, async);
-    }
-
-    public ITask(@NotNull P plugin, long interval, boolean async) {
-        this.plugin = plugin;
-        this.interval = interval;
-        this.async = async;
     }
 
     public abstract void action();
@@ -26,32 +23,27 @@ public abstract class ITask<P extends CodexPlugin<P>> {
     public void start() {
         if (this.interval <= 0) return;
 
-        if (async) {
-            this.async();
-        } else {
-            this.sync();
-        }
+        if (async) this.async();
+        else this.sync();
     }
 
     private void sync() {
-        this.id = plugin.getServer().getScheduler().runTaskTimer(plugin, () -> {
-            this.action();
-        }, 1L, interval).getTaskId();
+        this.id = plugin.getServer()
+                .getScheduler()
+                .runTaskTimer(plugin, this::action, 1L, interval)
+                .getTaskId();
     }
 
     private void async() {
-        this.id = plugin.getServer().getScheduler().runTaskTimerAsynchronously(plugin, () -> {
-            this.action();
-        }, 1L, interval).getTaskId();
+        this.id = plugin.getServer()
+                .getScheduler()
+                .runTaskTimerAsynchronously(plugin, this::action, 1L, interval)
+                .getTaskId();
     }
 
     public void stop() {
         if (this.interval <= 0) return;
 
         this.plugin.getServer().getScheduler().cancelTask(this.getId());
-    }
-
-    public int getId() {
-        return id;
     }
 }
