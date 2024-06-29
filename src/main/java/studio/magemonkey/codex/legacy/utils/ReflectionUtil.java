54ec01a845b370c9a716a20900d8b9f3a965af60
Sentropic <sentropic.sv@gmail.com>
2024-06-29 17:46:35 -0600

    Fix MC version detection

--- src/main/java/studio/magemonkey/codex/legacy/utils/ReflectionUtil.java
+++ src/main/java/studio/magemonkey/codex/legacy/utils/ReflectionUtil.java
@@ -5,16 +5,28 @@ import org.bukkit.Bukkit;
 
 public class ReflectionUtil {
     private static final String CRAFTBUKKIT_PACKAGE = Bukkit.getServer().getClass().getPackage().getName();
-
-    private static final String  VERSION       =
-            Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",")[3];
+    private static final String VERSION;
+    static {
+        String[] pack = Bukkit.getServer().getClass().getPackage().getName().replace(".", ",").split(",");
+        if (pack.length == 4) {
+            VERSION = pack[3];
+        } else {
+            String version = Bukkit.getVersion();
+            version = version.substring(0, version.indexOf('-')).replace('.', '_');
+            int i = version.lastIndexOf('_');
+            VERSION = 'v'+version.substring(0, i)+"_R"+(Integer.parseInt(version.substring(i+1))-1);
+        }
+    }
     public static final  int     MINOR_VERSION = Integer.parseInt(VERSION.split("_")[1]);
-    private static final String  version       = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
     @Getter
-    private static final boolean newVersion    = Integer.parseInt(version.split("_")[1]) >= 17;
+    private static final boolean newVersion    = Integer.parseInt(VERSION.split("_")[1]) >= 17;
 
     public static Class<?> getNMSClass(String name) throws ClassNotFoundException {
-        return Class.forName("net.minecraft.server." + version + "." + name);
+        try {
+            return Class.forName("net.minecraft.server." + name);
+        } catch (ClassNotFoundException e) {
+            return Class.forName("net.minecraft.server." + VERSION + "." + name);
+        }
     }
 
     public static Class<?> getBukkitClass(String name) throws ClassNotFoundException {
