ac433255e4d062d8ac372991118a0bd785a594a4
Travja <the.only.t.craft@gmail.com>
2024-11-28 14:40:14 -0700

    Revert "Dumb attributes being converted to an interface"
    This reverts commit e0fdc3275818f4a7e6b74c59cbcdea2c295a81a1.

--- src/main/java/studio/magemonkey/codex/util/AttributeUT.java
+++ src/main/java/studio/magemonkey/codex/util/AttributeUT.java
@@ -1,11 +1,10 @@
 package studio.magemonkey.codex.util;
 
 import lombok.SneakyThrows;
-import org.bukkit.Keyed;
+import org.bukkit.attribute.Attribute;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Objects;
 
 public class AttributeUT {
     private static Method valueOf;
@@ -14,34 +13,32 @@ public class AttributeUT {
      * Resolve an attribute by name. This method is deprecated because
      * the underlying method is also deprecated. This is a temporary fix
      * until we determine where we want to go.
-     *
      * @param name The name of the attribute
      * @return The attribute
-     * @deprecated This method is a hack that is necessary for backward compatibility with &lt;1.21.3 versions. It's
-     * annoying and should be removed when possible, though this will probably require a hard break at 1.21.3+.
+     * @deprecated
      */
     @SneakyThrows
     @Deprecated
-    public static Keyed resolve(String name) {
+    public static Attribute resolve(String name) {
         if (valueOf == null) {
-            valueOf = Objects.requireNonNull(Reflex.getClass("org.bukkit.attribute.Attribute")).getDeclaredMethod("valueOf", String.class);
+            valueOf = Attribute.class.getDeclaredMethod("valueOf", String.class);
         }
 
-        Keyed attribute = null;
+        Attribute attribute = null;
         try {
-            attribute = (Keyed) valueOf.invoke(null, name);
+            attribute = (Attribute) valueOf.invoke(null, name);
         } catch (InvocationTargetException ignored) {
         }
 
         if (attribute == null) {
             try {
-                attribute = (Keyed) valueOf.invoke(null, "GENERIC_" + name);
+                attribute = (Attribute) valueOf.invoke(null, "GENERIC_" + name);
             } catch (InvocationTargetException ignored) {
             }
         }
         if (attribute == null) {
             try {
-                attribute = (Keyed) valueOf.invoke(null, "PLAYER_" + name);
+                attribute = (Attribute) valueOf.invoke(null, "PLAYER_" + name);
             } catch (InvocationTargetException ignored) {
             }
         }
--- src/main/java/studio/magemonkey/codex/util/reflection/Reflection_1_18.java
+++ src/main/java/studio/magemonkey/codex/util/reflection/Reflection_1_18.java
@@ -1,6 +1,7 @@
 package studio.magemonkey.codex.util.reflection;
 
 import com.google.common.collect.Multimap;
+import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
@@ -28,24 +29,23 @@ public class Reflection_1_18 extends Reflection_1_17 {
     @Override
     public Object getGenericAttribute(String name) {
         try {
-            return AttributeUT.resolve(name);
+            return Attribute.valueOf("GENERIC_" + name);
         } catch (IllegalArgumentException e) {
             return null;
         }
     }
 
     @Override
-    @SuppressWarnings("rawtypes")
-    public Multimap getAttributes(@NotNull ItemStack itemStack) {
+    public Multimap<Attribute, AttributeModifier> getAttributes(@NotNull ItemStack itemStack) {
         EquipmentSlot slot = itemStack.getType().getEquipmentSlot();
         return itemStack.getType().getDefaultAttributeModifiers(slot);
     }
 
     @Override
-    @SuppressWarnings({"rawtypes", "unchecked"})
     public double getAttributeValue(@NotNull ItemStack item, @NotNull Object attribute) {
-        Multimap                      modifiers = getAttributes(item);
-        Collection<AttributeModifier> mod       = modifiers.get(attribute);
+        Attribute                              attr      = (Attribute) attribute;
+        Multimap<Attribute, AttributeModifier> modifiers = getAttributes(item);
+        Collection<AttributeModifier>          mod       = modifiers.get(attr);
         if (mod.isEmpty()) return 0;
 
         return mod.stream().mapToDouble(AttributeModifier::getAmount).sum();
