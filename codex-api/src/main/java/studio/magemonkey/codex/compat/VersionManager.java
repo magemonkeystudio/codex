5d0f86e101ac61c244922e9dd80e84f2ac5fe46b
Travja <the.only.t.craft@gmail.com>
2025-01-09 23:58:53 -0700

    Add support for 1.20.1 back

--- codex-api/src/main/java/studio/magemonkey/codex/compat/VersionManager.java
+++ codex-api/src/main/java/studio/magemonkey/codex/compat/VersionManager.java
@@ -43,13 +43,14 @@ public class VersionManager {
 
     private static String getPackageFromVersion(String version) {
         return switch (version) {
-            case "1.18", "1.18.1", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.20", "1.20.1" ->
+            case "1.18", "1.18.1", "1.19", "1.19.1", "1.19.2", "1.19.3", "1.20" ->
                     throw new UnsupportedVersionException("Version " + version
                             + " is not supported. Please upgrade to the latest minor version of your current major version.");
             case "1.16.5" -> "v1_16_5";
             case "1.17", "1.17.1" -> "v1_17";
             case "1.18.2" -> "v1_18_2";
             case "1.19.4" -> "v1_19_4";
+            case "1.20.1" -> "v1_20_1";
             case "1.20.2" -> "v1_20_2";
             case "1.20.3", "1.20.4" -> "v1_20_4";
             case "1.20.5", "1.20.6" -> "v1_20_6";
---
+++ codex-nms/codex-nms-v1_20_1/src/main/java/studio/magemonkey/codex/nms/v1_20_1/CompatImpl.java
@@ -0,0 +1,42 @@
+package studio.magemonkey.codex.nms.v1_20_1;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.attribute.AttributeModifier;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import studio.magemonkey.codex.api.meta.NBTAttribute;
+import studio.magemonkey.codex.compat.Compat;
+
+public class CompatImpl implements Compat {
+    @Override
+    public AttributeModifier createAttributeModifier(NBTAttribute attribute,
+                                                     double amount,
+                                                     AttributeModifier.Operation operation) {
+        return new AttributeModifier(ATTRIBUTE_BONUS_UUID, attribute.getNmsName(), amount, operation);
+    }
+
+    @Override
+    public String getAttributeKey(AttributeModifier attributeModifier) {
+        return attributeModifier.getName();
+    }
+
+    @Override
+    public String getItemName(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) {
+            return item.getType().toString();
+        }
+
+        String name = null;
+
+        if (meta.hasDisplayName()) name = meta.getDisplayName();
+        if (name == null && meta.getLore() != null && !meta.getLore().isEmpty()) name = meta.getLore().get(0);
+        if (name == null) name =
+                LegacyComponentSerializer.legacyAmpersand()
+                        .serializeOrNull(Component.translatable(item.getTranslationKey()));
+
+        //noinspection ConstantValue
+        return name == null ? item.getType().toString() : name;
+    }
+}
