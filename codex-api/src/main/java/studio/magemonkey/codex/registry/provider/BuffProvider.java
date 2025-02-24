bf2ed61d7cc25b32a096bb3e2b5babceb0cdb303
Travja <the.only.t.craft@gmail.com>
2025-02-24 01:26:58 -0700

    Improvements to Buff system

--- codex-api/src/main/java/studio/magemonkey/codex/registry/provider/BuffProvider.java
+++ codex-api/src/main/java/studio/magemonkey/codex/registry/provider/BuffProvider.java
@@ -3,5 +3,26 @@ package studio.magemonkey.codex.registry.provider;
 import org.bukkit.entity.LivingEntity;
 
 public interface BuffProvider {
+    /**
+     * Scales a value for an entity
+     *
+     * @param name  name of the buff
+     * @param player entity to scale the value for
+     * @param value value to scale
+     * @return the scaled value
+     */
     double scaleValue(String name, LivingEntity player, double value);
+
+    /**
+     * Scales the defense relative to the damage being dealt
+     *
+     * @param name    name of the buff
+     * @param entity  entity to scale the defense for
+     * @param damage  damage being dealt
+     * @return the scaled defense
+     */
+    default double scaleDamageForDefense(String name, LivingEntity entity, double damage) {
+        // Without being overridden, this method should make no modification to the damage
+        return damage;
+    }
 }
--- codex-core/src/main/java/studio/magemonkey/codex/registry/BuffRegistry.java
+++ codex-core/src/main/java/studio/magemonkey/codex/registry/BuffRegistry.java
@@ -33,4 +33,28 @@ public class BuffRegistry {
 
         return scaled;
     }
+
+    /**
+     * Scales the damage dealt directly for the target entity. This method should be used when
+     * modifications to the damage need to be made outside the vanilla "defense" calculations.
+     * The primary example of this is Fabled's shield system. Damage is reduced directly instead
+     * of being defended. This is essentially used for absorption effects.
+     * <p>
+     * It is important to note that we are _specifically_ not using the {@link #scaleValue} method
+     * here, as this method will only scale the value if the {@link BuffProvider} overrides the implementation.
+     *
+     * @param name    name of the buff
+     * @param entity  entity to scale the damage for
+     * @param damage  damage being dealt
+     * @return the modified damage
+     */
+    public static double scaleDamageForDefense(String name, LivingEntity entity, double damage) {
+        double scaled = damage;
+
+        for (BuffProvider provider : PROVIDERS) {
+            scaled = provider.scaleDamageForDefense(name, entity, damage);
+        }
+
+        return scaled;
+    }
 }
