88cc144edc26f9bb1f3d322937234f17b5f6ee1d
Trav <the.only.t.craft@gmail.com>
2026-03-09 12:45:21 -0600

    Trigger Build

--- codex-plugin/src/main/java/studio/magemonkey/codex/CodexEngine.java
+++ codex-plugin/src/main/java/studio/magemonkey/codex/CodexEngine.java
@@ -58,7 +58,7 @@ import java.util.HashSet;
 import java.util.Hashtable;
 import java.util.Set;
 
-// Main Codex logic
+// Main Codex logic, such as config management, hook management, and manager setup
 public class CodexEngine extends CodexPlugin<CodexEngine> implements Listener {
     private static final Hashtable<String, Config> configs = new Hashtable<>();
     private static       CodexEngine               instance;
